b44a9c40da14a3b0535779152bb8812920f4908e
Rafael Omodei <rafael.omodei@dasa.com.br>
2023-11-06 11:00:16 -0300

    feat: added icons animated

---
+++ src/assets/lordIcon/index.ts
@@ -0,0 +1,13 @@
+import UploadFileJson from './uploadFile.json';
+import CheckSuccessJson from './checkSuccessJson.json';
+import ErrorJson from './errorJson.json';
+import AnalyticsJson from './analyticsJson.json';
+
+const LORD_ICON = {
+  UploadFile: UploadFileJson,
+  CheckSuccess: CheckSuccessJson,
+  Error: ErrorJson,
+  Analytics: AnalyticsJson,
+};
+
+export { LORD_ICON };
